66f583e33eadb643017dc4bf799cfcf45406c7e4
jamesjenkinsjr <jamesjenkinsjr@icloud.com>
2019-11-08 10:15:56 -0500

    add exercise file

---
+++ exercise.js
@@ -0,0 +1,6 @@
+// 1. Create a stack class
+// Walk through the Stack class in the curriculum and understand it well. 
+// Then write a Stack class with its core functions (push, pop) from scratch.
+
+// - Create a stack called starTrek and add Kirk, Spock, McCoy, and Scotty to the stack.
+
